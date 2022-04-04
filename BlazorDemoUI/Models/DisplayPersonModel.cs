ced40d0e18fdecbf02e555bd3fe1ddd28c4229cd
matthew m <matthew.muir@student.centralia.edu>
2022-04-04 13:16:19 -0700

    Add new person feature implemented

---
+++ BlazorDemoUI/Models/DisplayPersonModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorDemoUI.Models
+{
+    public class DisplayPersonModel
+    {
+        [Required]
+        [StringLength(50, ErrorMessage = "Too long. 50 max")]
+        [MinLength(2, ErrorMessage = "Too short. 2 min")]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Too long. 50 max")]
+        [MinLength(2, ErrorMessage = "Too short. 2 min")]
+        public string LastName { get; set; }
+        [Required]
+        [MinLength(6, ErrorMessage ="Too short. 10 min")]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
